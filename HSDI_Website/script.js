e840f7d03e8b5856e1ade6699d9a707177f57deb
Replit user <>
2022-10-20 16:11:05 +0000

    update

---
+++ HSDI_Website/script.js
@@ -0,0 +1,17 @@
+// console.log('the javascript runs')
+// //document.body.addEventListener('load', function() {
+//   console.log('window loads')
+//   if(window.location.href.includes("About.html")) {
+//     document.getElementById('tab3').classList.add('active-background-color');
+//     console.log('about page loads')
+//   }
+
+//   else if(window.location.href.includes("Websites.html")) {
+//     document.getElementById('tab2').classList.add('active-background-color');
+//     console.log('websites page loads')
+//   }
+//   else if(window.location.href.includes("index.html")) {
+//     document.getElementById('tab1').classList.add('active-background-color');
+//     console.log('index page loads')
+//   }
+
